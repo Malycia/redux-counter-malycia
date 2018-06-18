43e9b9960363c4f46620ed417d74f5c0b80155f4
Virginie Gruber <virginie.gruber@gmail.com>
2018-06-18 10:04:10 +0200

    redux counter

--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -2,36 +2,12 @@ import React from 'react';
 
 import store from '~/store';
 
-class Counter extends React.Component {
-  // On veut gérer ici la connexion au state global (de l'application)
-  // Le composant Counter est intéressé par *certaines* parties du state.
-  // const state = store.getState();
-  state = {} // état local
-
-  componentDidMount() {
-    // Dès le départ, on commence à écouter les futures/potentielles
-    // modifications du state global.
-    store.subscribe(this.syncState);
-    // En plus, au démarrage, on synchronise les states.
-    this.syncState();
-  }
-
-  syncState = () => {
-    const globalState = store.getState(); // on récupère l'état global (de l'application)
-    // On stocke dans l'état local (du composant) l'information qui nous intéresse
-    this.setState({ // Ça déclenche un render
-      nbVoteTotal: globalState.oui + globalState.non,
-    });
-  }
-
-  render() {
-    const { nbVoteTotal } = this.state;
-    return (
+const Counter = ({ nbVoteTotal }) => {
+    (
       <div className="app-counter">
         {nbVoteTotal}
       </div>
     );
-  }
 }
 
 export default Counter;
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -3,20 +3,11 @@ import { connect } from 'react-redux';
 import Counter from '~/components/Counter';
 
 const mapStateToProps = (state, ownProps) => ({
-  name: ownProps.name,
-  nbVote: state[ownProps.name],
+  nbVoteTotal: state.oui + state.non,
 });
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
-  vote: () => {
-    dispatch({ type: 'VOTE', name: ownProps.name });
-  },
-});
 
 // connect : usine à composant React de type container
-const CounterContainer = connect(
-  mapStateToProps, // lecture du state => on génère des props pour Button
-  mapDispatchToProps, // écriture dans le state => on génère des props pour Button
-)(Counter);
+const CounterContainer = connect(mapStateToProps)(Counter);
 
 export default CounterContainer;
